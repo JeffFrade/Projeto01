1559e3a0fc6258a909e0f9965008f1ef08470ae4
Jefferson Frade <jefferrson.frade@gmail.com>
2017-11-03 03:48:02 -0200

    Acertando a index.php (Ainda Pode Sofrer Alterações)

--- public/index.php
+++ public/index.php
@@ -175,7 +175,13 @@
 </main>
 
 <footer>
-    <h4 class="text-center">Minha Loja - <?= date('Y')?> - Todos Direitos Reservados</h4>
+    <div class="footer1">
+        <h4 class="text-center">Minha Loja - <?= date('Y')?> - Todos Direitos Reservados</h4>
+    </div>
+
+    <div class="footer2">
+        <h4 class="text-center">Minha Loja - <?= date('Y')?> - Todos Direitos Reservados</h4>
+    </div>
 </footer>
 
 <?php
