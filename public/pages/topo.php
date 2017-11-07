06465f29ebefae92b555f836c0c9fcecd7a66d4b
Jefferson Frade <jefferrson.frade@gmail.com>
2017-11-07 19:01:34 -0200

    Adicionando Plugin do Facebook

--- public/pages/topo.php
+++ public/pages/topo.php
@@ -3,6 +3,18 @@
 ?>
 
 <body class="branco" id="app">
+<!-- Facebook: -->
+<div id="fb-root"></div>
+<script>
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.10';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
+<!-- Fim Facebook -->
 <?php
 //Menu:
 require_once 'menu.php';
