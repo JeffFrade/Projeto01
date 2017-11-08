e811afd3e90a4caaa23054465a398e0c95b26988
Jefferson Frade <jefferrson.frade@gmail.com>
2017-11-08 17:52:30 -0200

    Criando Método de Contagem de Prdutos e Implementando

--- DB.sql
+++ DB.sql
@@ -1,62 +1,206 @@
-CREATE DATABASE Loja01;
-USE Loja01;
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: loja01
+-- ------------------------------------------------------
+-- Server version	5.5.54
 
-CREATE TABLE cliente(
-  cpf CHAR(14) PRIMARY KEY,
-  nome VARCHAR(170) NOT NULL,
-  dataNasc DATE NOT NULL,
-  email VARCHAR(170) NOT NULL,
-  telefone CHAR(14),
-  celular VARCHAR(15),
-  cep CHAR(9) NOT NULL,
-  endereco VARCHAR(250) NOT NULL,
-  cidade VARCHAR(250) NOT NULL,
-  estado VARCHAR(25) NOT NULL,
-  numero INT NOT NULL,
-  complemento VARCHAR(50),
-  senha VARCHAR(255) NOT NULL);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-ALTER TABLE cliente ADD bairro VARCHAR(100);
+--
+-- Table structure for table `banners`
+--
 
-CREATE TABLE categoria(
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  categoria VARCHAR(50) NOT NULL);
+DROP TABLE IF EXISTS `banners`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `banners` (
+  `id` int(11) NOT NULL,
+  `url` varchar(5000) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE produtos(
-  cod INT PRIMARY KEY AUTO_INCREMENT,
-  item VARCHAR(200) NOT NULL,
-  categoria INT NOT NULL,
-  FOREIGN KEY (categoria) REFERENCES categoria(id),
-  descricao VARCHAR(3500) NOT NULL,
-  preco FLOAT NOT NULL,
-  vitrine CHAR(1) NOT NULL,
-  qtde INT NOT NULL,
-  imagem VARCHAR(2000) NOT NULL);
+--
+-- Table structure for table `carrinho`
+--
 
-CREATE TABLE carrinho(
-  cpf CHAR(14) PRIMARY KEY,
-  FOREIGN KEY (cpf) REFERENCES cliente(cpf),
-  cod_prod INT NOT NULL,
-  FOREIGN KEY (cod_prod) REFERENCES produtos(cod),
-  qtde INT NOT NULL,
-  total FLOAT NOT NULL);
+DROP TABLE IF EXISTS `carrinho`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `carrinho` (
+  `cpf` char(14) NOT NULL,
+  `cod_prod` int(11) NOT NULL,
+  `qtde` int(11) NOT NULL,
+  `total` float NOT NULL,
+  PRIMARY KEY (`cpf`),
+  KEY `cod_prod` (`cod_prod`),
+  CONSTRAINT `carrinho_ibfk_1` FOREIGN KEY (`cpf`) REFERENCES `cliente` (`cpf`),
+  CONSTRAINT `carrinho_ibfk_2` FOREIGN KEY (`cod_prod`) REFERENCES `produtos` (`cod`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE venda(
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  cpf CHAR(14) NOT NULL,
-  FOREIGN KEY (cpf) REFERENCES carrinho(cpf),
-  valor FLOAT NOT NULL);
+--
+-- Table structure for table `categoria`
+--
 
-CREATE TABLE funcionario(
-  usuario VARCHAR(50) PRIMARY KEY,
-  senha VARCHAR(255) NOT NULL,
-  perfil VARCHAR(50) NOT NULL);
+DROP TABLE IF EXISTS `categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categoria` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `categoria` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE `log`(
-  usuario VARCHAR(50) NOT NULL,
-  FOREIGN KEY (usuario) REFERENCES funcionario(usuario),
-  cod_prod INT NOT NULL,
-  FOREIGN KEY (cod_prod) REFERENCES produtos(cod),
-  `data` DATE NOT NULL,
-  hora TIME NOT NULL,
-  tipo VARCHAR(25) NOT NULL);
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cliente` (
+  `cpf` char(14) NOT NULL,
+  `nome` varchar(170) NOT NULL,
+  `dataNasc` date NOT NULL,
+  `email` varchar(170) NOT NULL,
+  `telefone` char(14) DEFAULT NULL,
+  `celular` varchar(15) DEFAULT NULL,
+  `cep` char(9) NOT NULL,
+  `endereco` varchar(250) NOT NULL,
+  `cidade` varchar(250) NOT NULL,
+  `estado` varchar(25) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `complemento` varchar(50) DEFAULT NULL,
+  `senha` varchar(255) NOT NULL,
+  `bairro` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`cpf`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `conteudo`
+--
+
+DROP TABLE IF EXISTS `conteudo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `conteudo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(25) NOT NULL,
+  `conteudo` varchar(2500) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `funcionario`
+--
+
+DROP TABLE IF EXISTS `funcionario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `funcionario` (
+  `usuario` varchar(50) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `perfil` varchar(50) NOT NULL,
+  PRIMARY KEY (`usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `log`
+--
+
+DROP TABLE IF EXISTS `log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `log` (
+  `usuario` varchar(50) NOT NULL,
+  `cod_prod` int(11) NOT NULL,
+  `data` date NOT NULL,
+  `hora` time NOT NULL,
+  `tipo` varchar(25) NOT NULL,
+  KEY `usuario` (`usuario`),
+  KEY `cod_prod` (`cod_prod`),
+  CONSTRAINT `log_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `funcionario` (`usuario`),
+  CONSTRAINT `log_ibfk_2` FOREIGN KEY (`cod_prod`) REFERENCES `produtos` (`cod`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `mensagens`
+--
+
+DROP TABLE IF EXISTS `mensagens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mensagens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(170) NOT NULL,
+  `email` varchar(170) NOT NULL,
+  `mensagem` varchar(4500) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `produtos`
+--
+
+DROP TABLE IF EXISTS `produtos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `produtos` (
+  `cod` int(11) NOT NULL AUTO_INCREMENT,
+  `item` varchar(200) NOT NULL,
+  `categoria` int(11) NOT NULL,
+  `descricao` varchar(3500) NOT NULL,
+  `preco` float NOT NULL,
+  `vitrine` char(1) NOT NULL,
+  `qtde` int(11) NOT NULL,
+  `imagem` varchar(2000) NOT NULL,
+  PRIMARY KEY (`cod`),
+  KEY `categoria` (`categoria`),
+  CONSTRAINT `produtos_ibfk_1` FOREIGN KEY (`categoria`) REFERENCES `categoria` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `venda`
+--
+
+DROP TABLE IF EXISTS `venda`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `venda` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cpf` char(14) NOT NULL,
+  `valor` float NOT NULL,
+  `status` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cpf` (`cpf`),
+  CONSTRAINT `venda_ibfk_1` FOREIGN KEY (`cpf`) REFERENCES `carrinho` (`cpf`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-11-08 17:52:02
